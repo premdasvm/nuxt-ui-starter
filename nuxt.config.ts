367b309b1da6c708062a0876cec1899cdf26577e
Prem <premdasvm8@gmail.com>
2025-05-22 14:58:08 +0530

    refactor: replaced `@nuxt/ui-pro` with `@nuxt/ui`.  - removed the patch script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/ui-pro", "@nuxt/eslint"],
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
 
   devtools: { enabled: true },
 
